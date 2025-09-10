01a7e37bdc42f06c755041ee1b8f33eb3f57eab7
ice breaker <1324318532@qq.com>
2025-09-10 22:16:03 +0800

    chore: upgrade

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,20 +1,22 @@
-import path from 'node:path'
 import { defineConfig } from 'vitest/config'
 
-export default defineConfig({
-  test: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, './src'),
+export default defineConfig(
+  () => {
+    return {
+      test: {
+        projects: [
+          'packages/*',
+          'apps/*',
+        ],
+        coverage: {
+          enabled: true,
+          all: false,
+          skipFull: true,
+        },
+        forceRerunTriggers: [
+          '**/{vitest,vite}.config.*/**',
+        ],
       },
-    ],
-    globals: true,
-    coverage: {
-      enabled: true,
-      include: ['src/**'],
-    },
-    testTimeout: 60_000,
-    setupFiles: ['./vitest.setup.ts'],
+    }
   },
-})
+)
